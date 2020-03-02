b8f47c2467e0f36ead00dddafc94dc962a70d5b9
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2020-03-02 14:42:25 +0100

    Add more benchmarks

--- benchmark/index.ts
+++ benchmark/index.ts
@@ -1,23 +1,49 @@
 'use strict';
 import {URL} from 'url';
 import https = require('https');
+import axios from 'axios';
 import Benchmark = require('benchmark');
-// @ts-ignore No types
+import fetch from 'node-fetch';
 import request = require('request');
 import got from '../source';
+import PromisableRequest from '../source/as-promise/core';
 
 // Configuration
+const httpsAgent = new https.Agent({
+	keepAlive: true,
+	rejectUnauthorized: false
+});
+
 const url = new URL('https://127.0.0.1:8080');
 const urlString = url.toString();
-const options = {
+
+const gotOptions = {
 	agent: {
-		https: new https.Agent({keepAlive: true})
+		https: httpsAgent
 	},
 	rejectUnauthorized: false
 };
 
 const requestOptions = {
-	strictSSL: false
+	strictSSL: false,
+	agent: httpsAgent
+};
+
+const fetchOptions = {
+	agent: httpsAgent
+};
+
+const axiosOptions = {
+	url: urlString,
+	httpsAgent,
+	rejectUnauthorized: false
+};
+
+fetchOptions;requestOptions;got;fetch;request;
+
+const axiosStreamOptions: typeof axiosOptions & {responseType: 'stream'} = {
+	...axiosOptions,
+	responseType: 'stream'
 };
 
 const suite = new Benchmark.Suite();
@@ -26,13 +52,21 @@ const suite = new Benchmark.Suite();
 suite.add('got - promise', {
 	defer: true,
 	fn: async (deferred: {resolve(): void}) => {
-		await got(url, options);
+		await got(url, gotOptions);
 		deferred.resolve();
 	}
 }).add('got - stream', {
 	defer: true,
 	fn: async (deferred: {resolve(): void}) => {
-		got.stream(url, options).resume().once('end', () => {
+		got.stream(url, gotOptions).resume().once('end', () => {
+			deferred.resolve();
+		});
+	}
+}).add('got - core', {
+	defer: true,
+	fn: async (deferred: {resolve(): void}) => {
+		const stream = new PromisableRequest(url, gotOptions);
+		stream.resume().once('end', () => {
 			deferred.resolve();
 		});
 	}
@@ -47,6 +81,48 @@ suite.add('got - promise', {
 			deferred.resolve();
 		});
 	}
+}).add('request - stream', {
+	defer: true,
+	fn: (deferred: {resolve(): void}) => {
+		const stream = request(urlString, requestOptions);
+		stream.resume();
+		stream.once('end', () => {
+			deferred.resolve();
+		});
+	}
+}).add('node-fetch - promise', {
+	defer: true,
+	fn: async (deferred: {resolve(): void}) => {
+		const response = await fetch(url, fetchOptions);
+		await response.text();
+
+		deferred.resolve();
+	}
+}).add('node-fetch - stream', {
+	defer: true,
+	fn: async (deferred: {resolve(): void}) => {
+		const {body} = await fetch(url, fetchOptions);
+
+		body.resume();
+		body.once('end', () => {
+			deferred.resolve();
+		});
+	}
+}).add('axios - promise', {
+	defer: true,
+	fn: async (deferred: {resolve(): void}) => {
+		await axios.request(axiosOptions);
+		deferred.resolve();
+	}
+}).add('axios - stream', {
+	defer: true,
+	fn: async (deferred: {resolve(): void}) => {
+		const {data} = await axios.request(axiosStreamOptions);
+		data.resume();
+		data.once('end', () => {
+			deferred.resolve();
+		});
+	}
 }).on('cycle', (event: Benchmark.Event) => {
 	console.log(String(event.target));
 }).on('complete', function (this: any) {
@@ -54,7 +130,13 @@ suite.add('got - promise', {
 }).run();
 
 // Results (i7-7700k, CPU governor: performance):
-// got - promise      x 3,296 ops/sec ±4.39% (78 runs sampled)
-// got - stream       x 4,876 ops/sec ±2.38% (81 runs sampled)
-// request - callback x 1,013 ops/sec ±3.66% (79 runs sampled)
-// Fastest is got - stream
+// got - promise        x 3,001 ops/sec ±6.78% (68 runs sampled)
+// got - stream         x 4,578 ops/sec ±4.47% (76 runs sampled)
+// got - core           x 5,717 ops/sec ±3.38% (78 runs sampled)
+// request - callback   x 7,165 ops/sec ±8.21% (75 runs sampled)
+// request - stream     x 7,709 ops/sec ±6.00% (78 runs sampled)
+// node-fetch - promise x 7,018 ops/sec ±4.36% (69 runs sampled)
+// node-fetch - stream  x 8,173 ops/sec ±1.97% (82 runs sampled)
+// axios - promise      x 6,338 ops/sec ±5.49% (70 runs sampled)
+// axios - stream       x 8,106 ops/sec ±4.89% (76 runs sampled)
+// Fastest is node-fetch - stream, request - stream
